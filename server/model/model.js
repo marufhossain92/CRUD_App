4a0c212a32b075c92c064daf8440bf3205535f4a
Maruf Hossain <70686053+maruf-cyberspace@users.noreply.github.com>
2022-10-04 01:43:36 +0600

    first commit

---
+++ server/model/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    gender: String,
+    status: String
+});
+
+const UserDb = mongoose.model('userdb', userSchema);
+
+module.exports = UserDb;
